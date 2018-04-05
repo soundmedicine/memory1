0818f1a4b9f65e887fc01e8abfc78478c9bf7aaa
Roberto Santos <robertosantos@Robertos-MBP.PK5001Z>
2018-04-05 17:43:47 -0600

    add queries :sparkles:

--- seeds/01_resolution.js
+++ seeds/01_resolution.js
@@ -5,15 +5,15 @@ exports.seed = function(knex, Promise) {
       let resolutions = [{
         id: 1,
         dueDate: '1997-02-01',
-        resolution: 'Go skiing!'
+        resolution: 'Go skiing'
       }, {
         id: 2,
         dueDate: '1997-05-01',
-        resolution: 'Do stand-up!'
+        resolution: 'Do stand-up'
       }, {
         id: 3,
         dueDate: '1997-09-01',
-        resolution: 'Start knitting!'
+        resolution: 'Start knitting'
       }]
 
       return knex('resolution').insert(resolutions)
